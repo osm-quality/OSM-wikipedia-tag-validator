6c7c27c5695c0aca18c919bad58ccbeafdcca98d
Mateusz Konieczny <matkoniecz@gmail.com>
2015-03-30 21:39:55 +0200

    fix formatting, minor renames and clarification

--- test.py
+++ test.py
@@ -93,8 +93,8 @@ def output_element(element, message):
         lon_m = str(lon_m)
         lon_s = str(lon_s)
 
-        lat = "%.4f" % lat  #drop overprecision
-        lon = "%.4f" % lon  #drop overprecision
+        lat = "%.4f" % lat  # drop overprecision
+        lon = "%.4f" % lon  # drop overprecision
 
         print lat
         print lon
@@ -192,8 +192,8 @@ def process_element(element, forced_refresh=False):
     else:
         language_code = get_language_code_from_link(link)
         article_name = get_article_name_from_link(link)
-        if args.expected_language is not None and args.expected_language != language_code:
-            output_element(element, "missing " + args.expected_language + ":")
+        if args.expected_language_code is not None and args.expected_language_code != language_code:
+            output_element(element, "missing " + args.expected_language_code + ":")
             return True
         if language_code is None or language_code.__len__() > 2:
             output_element(element, "malformed wikipedia tag (" + link + ")")
@@ -206,7 +206,7 @@ def process_element(element, forced_refresh=False):
         code_file = open(get_filename_with_code(language_code, article_name), 'r')
         code = int(code_file.read())
         code_file.close()
-        #<span class="latitude">50°04'02”N</span>&#160;<span class="longitude">19°55'03”E</span>
+        # <span class="latitude">50°04'02”N</span>&#160;<span class="longitude">19°55'03”E</span>
         index = string.find(page, "<span class=\"latitude\">")
         inline = string.find(page, "coordinates inline plainlinks")
         if index > inline != -1:
@@ -230,11 +230,14 @@ def process_element(element, forced_refresh=False):
 
 def parsed_args():
     parser = argparse.ArgumentParser(description='Validation of wikipedia tag in osm data.')
-    parser.add_argument('-expected_language', '-l', dest='expected_language', type=str, help='expected language code')
+    parser.add_argument('-expected_language_code', '-l', dest='expected_language_code', type=str, help='expected language code')
     parser.add_argument('-file', '-f', dest='file', type=str, help='location of osm file')
-    parser.add_argument('-lat', '-latitude', dest='lat', type=float, help='location of area, OSM data will be fetched')
-    parser.add_argument('-lon', '-longitude', dest='lon', type=float, help='location of area, OSM data will be fetched')
-    parser.add_argument('-delta', '-d', dest='delta', type=float, help='size of area, OSM data will be fetched')
+    parser.add_argument('-lat', '-latitude', dest='lat', type=float,
+                        help='location of area, OSM data will be fetched (in deegres)')
+    parser.add_argument('-lon', '-longitude', dest='lon', type=float,
+                        help='location of area, OSM data will be fetched (in deegres)')
+    parser.add_argument('-delta', '-d', dest='delta', type=float,
+                        help='size of area, OSM data will be fetched (in deegres)')
     parser.add_argument('-flush_cache', dest='flush_cache', help='adding this parameter will trigger flushing cache',
                         action='store_true')
     parser.add_argument('-flush_cache_for_reported_situations', dest='flush_cache_for_reported_situations',
@@ -273,7 +276,7 @@ def get_coords_of_complex_object(element, node_database, way_database):
                 if node_database[node_id] is None:
                     raise KeyError
             except KeyError:
-                return None  #node outside of downloaded map
+                return None  # node outside of downloaded map
             lat = node_database[node_id].lat
             lon = node_database[node_id].lon
         elif tag.tag == "member" and tag.attrib['type'] == "way":
@@ -282,7 +285,7 @@ def get_coords_of_complex_object(element, node_database, way_database):
                 if way_database[way_id] is None:
                     raise KeyError
             except KeyError:
-                return None  #way outside of downloaded map
+                return None  # way outside of downloaded map
             lat = way_database[way_id].lat
             lon = way_database[way_id].lon
         else:
@@ -298,6 +301,7 @@ class Coord:
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
+
 
 args = parsed_args()
 if args.lat and args.lon:
