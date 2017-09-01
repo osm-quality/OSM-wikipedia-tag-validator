2c9e8773c738a86bb937c68ffd32cae855b2e980
Mateusz Konieczny <matkoniecz@gmail.com>
2017-09-01 22:58:06 +0200

    more helpful messages

--- wikipedia_validator.py
+++ wikipedia_validator.py
@@ -73,7 +73,7 @@ def check_for_wikipedia_wikidata_collision(present_wikidata_id, language_code, a
             message = "wikidata and wikipedia tags link to a different objects, because wikipedia page points toward redirect that should be followed (" + compare_wikidata_ids(present_wikidata_id, wikidata_id_from_article) +")"
             new_wikipedia_link = language_code+":"+title_after_possible_redirects
             return ErrorReport(error_id = "wikipedia wikidata mismatch - follow redirect", error_message = message, desired_wikipedia_target = new_wikipedia_link)
-    message = "wikidata and wikipedia tags link to a different objects (" + compare_wikidata_ids(present_wikidata_id, wikidata_id_from_article) +")"
+    message = "wikidata and wikipedia tags link to a different objects (" + compare_wikidata_ids(present_wikidata_id, wikidata_id_from_article) +" wikidata from article)"
     return ErrorReport(error_id = "wikipedia wikidata mismatch", error_message = message)
 
 def compare_wikidata_ids(id1, id2):
