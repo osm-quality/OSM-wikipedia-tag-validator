3bc101f241c1b2ebd953ac251a6c5179bf929382
Mateusz Konieczny <matkoniecz@gmail.com>
2017-09-08 00:41:31 +0200

    split maproulette generation from query generation
    support also josm style querries

--- generate_overpass_query_output.py
+++ generate_overpass_query_output.py
@@ -1,21 +1,5 @@
 import argparse
-import yaml
-
-def get_write_location():
-    cache_location_config_filepath = 'cache_location.config'
-    cache_location_file = open(cache_location_config_filepath, 'r')
-    returned = cache_location_file.read()
-    cache_location_file.close()
-    return returned
-
-def load_data(yaml_report_filepath):
-    with open(yaml_report_filepath, 'r') as stream:
-        try:
-            return yaml.load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            return None
-    assert(False)
+import generate_shared
 
 def parsed_args():
     parser = argparse.ArgumentParser(description='Production of webpage about validation of wikipedia tag in osm data.')
@@ -25,26 +9,5 @@ def parsed_args():
         parser.error('Provide .osm file')
     return args
 
-def print_query_header():
-    print('[out:json];')
-    print('(')
-
-def print_query_footer():
-    print('); out body geom qt;')
-
-def main():
-    args = parsed_args()
-    print_query_header()
-    filename = args.file
-    reported_errors = load_data(get_write_location()+"/"+filename)
-    for e in reported_errors:
-        if e['error_id'] == 'wikipedia tag links to 404':
-            type = e['osm_object_url'].split("/")[3]
-            id = e['osm_object_url'].split("/")[4]
-            if type == "relation":
-                #relations skipped due to https://github.com/maproulette/maproulette2/issues/259
-                continue
-            print(type+'('+id+');')
-    print_query_footer()
-
-main()
+args = parsed_args()
+print generate_shared.get_query(filename = args.file, printed_error_ids = ['wikipedia tag links to 404'], format = "maproulette")
---
+++ generate_shared.py
@@ -0,0 +1,55 @@
+import yaml
+
+def get_write_location():
+    cache_location_config_filepath = 'cache_location.config'
+    cache_location_file = open(cache_location_config_filepath, 'r')
+    returned = cache_location_file.read()
+    cache_location_file.close()
+    return returned
+
+def load_data(yaml_report_filepath):
+    with open(yaml_report_filepath, 'r') as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return None
+    assert(False)
+
+def get_query_header(format):
+    header = ""
+    if format == "maproulette":
+        header+= '[out:json];'
+    elif format == "josm":
+        header += '[out:xml];'
+    else:
+        assert(False)
+    header += "\n"
+    header += '('
+    header += "\n"
+    return header
+
+def get_query_footer(format):
+    if format == "maproulette":
+        return '); out body geom qt;'
+    elif format == "josm":
+        return '); (._;>;); out meta qt;'
+    else:
+        assert(False)
+
+def get_query(filename, printed_error_ids, format):
+    # accepted formats:
+    # maproulette - json output, workarounds for maproulette bugs
+    # josm - xml output
+    returned = get_query_header(format)
+    reported_errors = load_data(get_write_location()+"/"+filename)
+    for e in reported_errors:
+        if e['error_id'] in printed_error_ids:
+            type = e['osm_object_url'].split("/")[3]
+            id = e['osm_object_url'].split("/")[4]
+            if type == "relation" and format == "maproulette":
+                #relations skipped due to https://github.com/maproulette/maproulette2/issues/259
+                continue
+            returned += type+'('+id+');' + "\n"
+    returned += get_query_footer(format)
+    return returned
