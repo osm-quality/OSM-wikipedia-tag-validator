4bae7f68f171ec8a8a68ad089ee40f27bb4c8a90
Mateusz Konieczny <matkoniecz@gmail.com>
2017-09-09 18:05:17 +0200

    include library file, move more functions to it

---
+++ common.py
@@ -0,0 +1,16 @@
+import html
+
+def get_file_storage_location():
+    cache_location_config_filepath = 'cache_location.config'
+    cache_location_file = open(cache_location_config_filepath, 'r')
+    returned = cache_location_file.read()
+    cache_location_file.close()
+    return returned
+
+def escape_from_internal_python_string_to_html_ascii(string):
+    return str(string).encode('ascii', 'xmlcharrefreplace').decode()
+
+def htmlify(string):
+    escaped = html.escape(string)
+    escaped_ascii = escape_from_internal_python_string_to_html_ascii(escaped)
+    return escaped_ascii.replace("\n", "<br />")
