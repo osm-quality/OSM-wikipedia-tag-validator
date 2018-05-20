6a4a4c508a0be99115d2b127850f714ad97f4dc5
Mateusz Konieczny <matkoniecz@gmail.com>
2018-05-20 10:15:59 +0200

    add entry to a blacklist

--- wikimedia_link_issue_reporter.py
+++ wikimedia_link_issue_reporter.py
@@ -169,6 +169,7 @@ class WikimediaLinkIssueDetector:
             'Q701338': {'prefix': 'brand:', 'expected_tags': {'shop': 'clothes'}, 'name': 'C&A'},
 
             'Q1160928': {'prefix': 'brand:', 'expected_tags': {'amenity': 'bank'}, 'name': 'mBank'},
+            'Q2003549': {'prefix': 'brand:', 'expected_tags': {'amenity': 'bank'}, 'name': 'Axis Bank'},
             #Q631047': {'prefix': 'brand:', 'expected_tags': {'amenity': 'bank'}, 'name': 'HDFC Bank'}, - see below, the same issue
             #'Q2003777': {'prefix': 'brand:', 'expected_tags': {'amenity': 'bank'}, 'name': 'Canara Bank'}, - retagged some, asked question on edit introducing them
 
