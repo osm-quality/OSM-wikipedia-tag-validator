dc6c9ad6715636f24abc1b8350392118a24fda7d
Mateusz Konieczny <matkoniecz@gmail.com>
2022-10-12 18:26:37 +0200

    split functions

--- script.py
+++ script.py
@@ -43,15 +43,6 @@ def create_table_if_needed(cursor):
                     (area_identifier text, filename text, download_type text, download_timestamp integer)''')
 
 def main():
-    issue_detector = wikimedia_link_issue_reporter.WikimediaLinkIssueDetector(
-        forced_refresh=False,
-        expected_language_code=None,
-        languages_ordered_by_preference=[],
-        additional_debug=False,
-        allow_requesting_edits_outside_osm=False,
-        allow_false_positives=False
-        )
-
     connection = sqlite3.connect(config.database_filepath())
     cursor = connection.cursor()
     if "osm_data" in existing_tables(cursor):
@@ -70,45 +61,65 @@ def main():
         if "hidden" in entry:
             if entry["hidden"] == True:
                 continue
-        website_main_title_part = entry['website_main_title_part']
-        merged_output_file = entry.get('merged_output_file', None)
-        language_code = entry.get('language_code', None)
-        identifier_of_region_for_overpass_query=entry['identifier']
-        downloaded_filepath = download.download_entry(internal_region_name, identifier_of_region_for_overpass_query)
-        load_osm_file.load_osm_file(downloaded_filepath, internal_region_name)
-
-        cursor = connection.cursor()
-        cursor.execute("SELECT rowid, type, id, lat, lon, tags, area_identifier, osm_data_updated, validator_complaint FROM osm_data WHERE area_identifier = :identifier", {"identifier": internal_region_name})
-        returned = cursor.fetchall()
-        for entry in returned:
-            rowid, object_type, object_id, lat, lon, tags, area_identifier, osm_data_updated, validator_complaint = entry
-            object_description = object_type + "/" + str(object_id)
-            tags = json.loads(tags)
-            location = (lat, lon)
-            reported = issue_detector.get_the_most_important_problem_generic(tags, location, object_type, object_description)
-            if reported != None:
-                link = "https://openstreetmap.org/" + object_type + "/" + str(object_id)
-                data = reported.data()
-                data['osm_object_url'] = link # TODO eliminate need for this
-                data['tags'] = tags # TODO eliminate need for this
-                data = json.dumps(data)
-                cursor.execute("UPDATE osm_data SET validator_complaint = :validator_complaint WHERE rowid = :rowid", {"validator_complaint": data, "rowid": rowid})
-            else:
-                cursor.execute("UPDATE osm_data SET validator_complaint = :validator_complaint WHERE rowid = :rowid", {"validator_complaint": "", "rowid": rowid})
-        connection.commit()
-
-        cursor.execute("SELECT rowid, type, id, lat, lon, tags, area_identifier, osm_data_updated, validator_complaint FROM osm_data WHERE area_identifier = :identifier AND validator_complaint IS NOT NULL AND validator_complaint <> ''", {"identifier": internal_region_name})
-        returned = cursor.fetchall()
-        reports = []
-        for entry in returned:
-            rowid, object_type, id, lat, lon, tags, area_identifier, osm_data_updated, validator_complaint = entry
-            tags = json.loads(tags)
-            validator_complaint = json.loads(validator_complaint)
-            reports.append(validator_complaint)
-        generate_webpage_with_error_output.generate_output_for_given_area(website_main_title_part, reports)
+        process_given_area(connection, entry)
     generate_webpage_with_error_output.write_index(cursor)
     connection.close()
     commit_changes_in_report_directory()
+
+
+def process_given_area(connection, entry):
+    # TODO: properly update by fetching new info about entries which were modified and entries currently carrying reports (as wikipedia tag could be simply removed!)
+    merged_output_file = entry.get('merged_output_file', None) # TODO! support this!
+    identifier_of_region_for_overpass_query=entry['identifier']
+    downloaded_filepath = download.download_entry(entry['internal_region_name'], identifier_of_region_for_overpass_query)
+    load_osm_file.load_osm_file(downloaded_filepath, entry['internal_region_name'])
+
+    cursor = connection.cursor()
+    update_validator_reports_for_given_area(cursor, entry['internal_region_name'], entry.get('language_code', None))
+    connection.commit()
+    generate_website_file_for_given_area(cursor, entry)
+
+def update_validator_reports_for_given_area(cursor, internal_region_name, language_code):
+    issue_detector = get_wikimedia_link_issue_reporter_object(language_code)
+    cursor.execute("SELECT rowid, type, id, lat, lon, tags, area_identifier, download_timestamp, validator_complaint FROM osm_data WHERE area_identifier = :identifier", {"identifier": internal_region_name})
+    returned = cursor.fetchall()
+    for entry in returned:
+        rowid, object_type, object_id, lat, lon, tags, area_identifier, download_timestamp, validator_complaint = entry
+        object_description = object_type + "/" + str(object_id)
+        tags = json.loads(tags)
+        location = (lat, lon)
+        reported = issue_detector.get_the_most_important_problem_generic(tags, location, object_type, object_description)
+        if reported != None:
+            link = "https://openstreetmap.org/" + object_type + "/" + str(object_id)
+            data = reported.data()
+            data['osm_object_url'] = link # TODO eliminate need for this
+            data['tags'] = tags # TODO eliminate need for this
+            data = json.dumps(data)
+            cursor.execute("UPDATE osm_data SET validator_complaint = :validator_complaint WHERE rowid = :rowid", {"validator_complaint": data, "rowid": rowid})
+        else:
+            cursor.execute("UPDATE osm_data SET validator_complaint = :validator_complaint WHERE rowid = :rowid", {"validator_complaint": "", "rowid": rowid})
+
+def get_wikimedia_link_issue_reporter_object(language_code):
+    return wikimedia_link_issue_reporter.WikimediaLinkIssueDetector(
+        forced_refresh=False,
+        expected_language_code=language_code, # may be None
+        languages_ordered_by_preference=[],
+        additional_debug=False,
+        allow_requesting_edits_outside_osm=False,
+        allow_false_positives=False
+        )
+
+def generate_website_file_for_given_area(cursor, entry):
+    website_main_title_part = entry['website_main_title_part']
+    cursor.execute("SELECT rowid, type, id, lat, lon, tags, area_identifier, download_timestamp, validator_complaint FROM osm_data WHERE area_identifier = :identifier AND validator_complaint IS NOT NULL AND validator_complaint <> ''", {"identifier": entry['internal_region_name']})
+    returned = cursor.fetchall()
+    reports = []
+    for entry in returned:
+        rowid, object_type, id, lat, lon, tags, area_identifier, download_timestamp, validator_complaint = entry
+        tags = json.loads(tags)
+        validator_complaint = json.loads(validator_complaint)
+        reports.append(validator_complaint)
+    generate_webpage_with_error_output.generate_output_for_given_area(website_main_title_part, reports)
 
 def commit_changes_in_report_directory():
     current_working_directory = os.getcwd()
