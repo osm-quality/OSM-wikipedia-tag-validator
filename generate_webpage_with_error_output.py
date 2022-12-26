94236af9cbba4a1f6dc871d6bfee5fae9f0e6fea
Mateusz Konieczny <matkoniecz@gmail.com>
2022-12-26 10:01:01 +0100

    review report types

--- generate_webpage_with_error_output.py
+++ generate_webpage_with_error_output.py
@@ -328,6 +328,8 @@ def for_review():
         'should use a secondary wikipedia tag - linking to a stampede',
         'should use a secondary wikipedia tag - linking to a festival',
         'should use a secondary wikipedia tag - linking to a fictional entity',
+        'should use a secondary wikipedia tag - linking to a type of structure',
+        'should use a secondary wikipedia tag - linking to a train category',
     ]
 
 def obvious_fixes():
@@ -347,10 +349,9 @@ def obvious_fixes():
 
 def for_tests():
     return [
-        'should use a secondary wikipedia tag - linking to a train category',
-        'should use a secondary wikipedia tag - linking to a biome (too generic, not linking specific object)',
-        'should use a secondary wikipedia tag - linking to a type of structure',
         'should use a secondary wikipedia tag - linking to a branch of military service',
+        'should use a secondary wikipedia tag - linking to an electronic device model series',
+        'should use a secondary wikipedia tag - linking to a history of a geographic region',
         # TODO detect when directly linked entry has https://www.wikidata.org/wiki/Property:P1282 set
         # TODO take down https://taginfo.openstreetmap.org/keys/related%3Awikipedia#chronology before it lays eggs
 
