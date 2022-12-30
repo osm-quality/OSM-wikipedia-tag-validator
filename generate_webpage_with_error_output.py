c1d7b12c90d750e63cbdd8d68e736bff51260aa5
Mateusz Konieczny <matkoniecz@gmail.com>
2022-12-30 17:44:29 +0100

    prepare new test wave

--- generate_webpage_with_error_output.py
+++ generate_webpage_with_error_output.py
@@ -366,6 +366,11 @@ def for_tests():
         'wikipedia tag unexpected language, article missing',
         'tag conflict with wikidata value - boring',
         'tag may be added based on wikidata - website', # dubious copyright
+        # multiple test cases created - enable it after fixing them
+        'should use a secondary wikipedia tag - linking to an art genre',
+
+        # enable after running out of art genre stuff
+        'should use a secondary wikipedia tag - linking to an event',
     ]
 
 def ignored():
@@ -373,14 +378,8 @@ def ignored():
         # delete this from database
         "should use a secondary wikipedia tag - linking to an industry",
 
-        # multiple test cases created - enable it after fixing them
-        'should use a secondary wikipedia tag - linking to an art genre',
-
         # enable after running out of art genre stuff and fixing reports after ferries
         'should use a secondary wikipedia tag - linking to a general industry',
-
-        # enable after running out of art genre stuff
-        'should use a secondary wikipedia tag - linking to an event',
 
         # too often problem is on Wikidata side - generate list and show it to Wikidata community?
         'should use a secondary wikipedia tag - linking to an uncoordinable generic object',
