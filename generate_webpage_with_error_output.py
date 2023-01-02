7662b2d20fd6aea1c4cb72fe2e293d80cbd6590f
Mateusz Konieczny <matkoniecz@gmail.com>
2023-01-02 21:19:02 +0100

    delete now gone remnants

--- generate_webpage_with_error_output.py
+++ generate_webpage_with_error_output.py
@@ -381,9 +381,6 @@ def ignored():
         # enable after running out of art genre stuff and fixing reports after ferries
         'should use a secondary wikipedia tag - linking to a general industry',
 
-        # delete this from database or check that it is now fully gone
-        "should use a secondary wikipedia tag - linking to an industry",
-
         # too often problem is on Wikidata side - generate list and show it to Wikidata community?
         'should use a secondary wikipedia tag - linking to an uncoordinable generic object',
 
