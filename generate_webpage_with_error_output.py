3e84158868cce5ded8371827f4ee3b5fe3fa386a
Mateusz Konieczny <matkoniecz@gmail.com>
2023-01-05 06:48:25 +0100

    promote tested report category

--- generate_webpage_with_error_output.py
+++ generate_webpage_with_error_output.py
@@ -303,6 +303,7 @@ def for_review():
         "malformed wikipedia tag - nonexisting language code",
         'information board with wikipedia tag, not subject:wikipedia',
         'information board with wikidata tag, not subject:wikidata',
+        'should use a secondary wikipedia tag - linking to an object that exists outside physical reality',
         'should use a secondary wikipedia tag - linking to a human',
         'should use a secondary wikipedia tag - linking to an animal or plant (and not an individual one)',
         'should use a secondary wikipedia tag - linking to a vehicle model or class',
@@ -354,8 +355,6 @@ def for_tests():
     return [
         'should use a secondary wikipedia tag - linking to a geodetic control network',
         'should use a secondary wikipedia tag - linking to a music festival',
-
-        'should use a secondary wikipedia tag - linking to an object that exists outside physical reality',
 
         # enable after running out of art genre stuff and fixing reports after ferries
         'should use a secondary wikipedia tag - linking to a general industry',
