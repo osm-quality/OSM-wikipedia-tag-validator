bcbaec2e60df8a77848823c3c3eed4ff931ad005
Mateusz Konieczny <matkoniecz@gmail.com>
2023-01-18 13:54:40 +0100

    processed new report types

--- generate_webpage_with_error_output.py
+++ generate_webpage_with_error_output.py
@@ -335,6 +335,8 @@ def for_review():
         'should use a secondary wikipedia tag - linking to an electronic device model series',
         'should use a secondary wikipedia tag - linking to a physical process',
         'malformed secondary wikidata tag',
+        'should use a secondary wikipedia tag - linking to a heraldic animal',
+        'should use a secondary wikipedia tag - linking to a general industry',
         'should use a secondary wikipedia tag - linking to a bicycle sharing system',
     ]
 
@@ -355,20 +357,14 @@ def obvious_fixes():
 
 def for_tests():
     return [
-        'secondary wikidata tag links to 404',
         'should use a secondary wikipedia tag - linking to a geodetic control network',
         'should use a secondary wikipedia tag - linking to a fireworks event',
-
-        # enable after running out of art genre stuff and fixing reports after ferries
-        'should use a secondary wikipedia tag - linking to a general industry',
-
 
         'should use a secondary wikipedia tag - linking to a branch of military service',
         'should use a secondary wikipedia tag - linking to a history of a geographic region',
         # TODO detect when directly linked entry has https://www.wikidata.org/wiki/Property:P1282 set
         # TODO take down https://taginfo.openstreetmap.org/keys/related%3Awikipedia#chronology before it lays eggs
 
-        'should use a secondary wikipedia tag - linking to a heraldic animal',
         'should use a secondary wikipedia tag - linking to a type of world view',
 
         # TODO: detect image=* that should be wikimedia_commons=*
@@ -379,6 +375,9 @@ def for_tests():
 
 def ignored():
     return [
+        # regenerate - all issues are fixed or false positives
+        'secondary wikidata tag links to 404',
+
         # multiple test cases created
         # enable it after Wikidata community runs out of what I reported
         # AND all tests in wikibrain are passing (flushing bad data + fixing that not-yet-detected case)
