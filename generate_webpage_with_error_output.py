2370f03d7303654e5416a188941de2249b663cf1
Mateusz Konieczny <matkoniecz@gmail.com>
2023-07-21 13:16:23 +0200

    new cutoff for test reports

--- generate_webpage_with_error_output.py
+++ generate_webpage_with_error_output.py
@@ -11,7 +11,7 @@ import obtain_from_overpass
 import database
 
 def skip_test_cases_before_timestamp():
-    return 1688637812
+    return 1689653097
 
 def generate_website_file_for_given_area(cursor, entry):
     reports = reports_for_given_area(cursor, entry['internal_region_name'])
